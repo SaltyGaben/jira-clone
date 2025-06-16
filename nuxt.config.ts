6f61129b7cc7ed8f536071f13075611776919a2c
SaltyGaben <hampus.tuvesson@protonmail.com>
2025-06-16 22:42:10 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+	compatibilityDate: '2025-05-15',
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/supabase', 'shadcn-nuxt'],
+	css: ['~/assets/css/tailwind.css'],
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	shadcn: {
+		prefix: '',
+		componentDir: './components/ui'
+	},
+	supabase: {
+		redirectOptions: {
+			login: '/login',
+			callback: '/confirm',
+			saveRedirectToCookie: true
+		}
+	}
+})
